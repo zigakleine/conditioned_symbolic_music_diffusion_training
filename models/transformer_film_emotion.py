6d17f986cbff7dfc0f6c5f9adb1de5018683a188
zigakleine <ziga.kleine@gmail.com>
2023-10-01 23:36:13 +0200

    transformer update

--- models/transformer_film_emotion.py
+++ models/transformer_film_emotion.py
@@ -17,15 +17,15 @@ class TransformerDDPME(nn.Module):
 
         self.num_timesteps = 1000
 
-        # self.embed_size = 2280
-        self.embed_size = 2048
+        self.embed_size = 2280
+        # self.embed_size = 2048
 
-        # self.num_heads = 12
-        self.num_heads = 8
+        self.num_heads = 12
+        # self.num_heads = 8
         self.num_layers = 6
 
-        # self.num_mlp_layers = 3
-        self.num_mlp_layers = 4
+        self.num_mlp_layers = 3
+        # self.num_mlp_layers = 4
         self.mlp_dims = 2048
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
