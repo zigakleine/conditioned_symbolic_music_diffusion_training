c3e1fd592e2537cc5bf13bfe218b0410f2e4ef36
zigakleine <ziga.kleine@gmail.com>
2023-10-07 22:37:24 +0200

    hehe

--- diffusion_main.py
+++ diffusion_main.py
@@ -188,7 +188,7 @@ def train():
             gpu_name = torch.cuda.get_device_name(i)
             print(f"GPU {i}: {gpu_name}")
 
-    lr = 2e-4
+    lr = 1e-3
     batch_size = 1
     current_dir = os.getcwd()
     to_save_dir = "/storage/local/ssd/zigakleine-workspace"
